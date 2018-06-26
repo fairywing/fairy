701cab1b625cf0d9060afbf7b18bb84e0bb1beec
fairy <1352425876@qq>
2018-06-26 18:35:56 +0800

    first commit

---
+++ fairy-dao/src/main/java/com/fairy/AA01.java
@@ -0,0 +1,81 @@
+package com.fairy;
+
+public class AA01 {
+    /**
+     * 数据库主键
+     */
+    private long   AAA001;
+    /**
+     * 登录名称
+     */
+    private String AAA002;
+
+    /**
+     * 密码
+     */
+    private String AAA003;
+
+    /**
+     * 注册手机号
+     */
+    private String AAA004;
+
+    /**
+     * 绑定邮箱地址
+     */
+    private String AAA005;
+
+    /**
+     * 有效标识
+     */
+    private long AAA006;
+
+
+    public long getAAA001() {
+        return AAA001;
+    }
+
+    public void setAAA001(long AAA001) {
+        this.AAA001 = AAA001;
+    }
+
+    public String getAAA002() {
+        return AAA002;
+    }
+
+    public void setAAA002(String AAA002) {
+        this.AAA002 = AAA002;
+    }
+
+    public String getAAA003() {
+        return AAA003;
+    }
+
+    public void setAAA003(String AAA003) {
+        this.AAA003 = AAA003;
+    }
+
+    public String getAAA004() {
+        return AAA004;
+    }
+
+    public void setAAA004(String AAA004) {
+        this.AAA004 = AAA004;
+    }
+
+    public String getAAA005() {
+        return AAA005;
+    }
+
+    public void setAAA005(String AAA005) {
+        this.AAA005 = AAA005;
+    }
+
+    public long getAAA006() {
+        return AAA006;
+    }
+
+    public void setAAA006(long AAA006) {
+        this.AAA006 = AAA006;
+    }
+}
